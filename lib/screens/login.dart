84f7f7f58b8d4fc60cb5ba1aa164f9d42e36762e
Leonardo Menti <leonardo.menti@studenti.unimi.it>
2021-12-02 17:01:12 +0100

    fix rendering error login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -82,38 +82,40 @@ class Login extends StatelessWidget {
       body: Center(
         child: Container(
           color: Color.fromARGB(255, 57, 97, 144),
-          padding: const EdgeInsets.all(35.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                height: 120.0,
-                child: Image.asset(
-                  "assets/images/logo.png",
-                  fit: BoxFit.contain,
+          child: Padding (
+            padding: const EdgeInsets.all(35.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 100.0,
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    fit: BoxFit.contain,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10.0),
-              Text("CALCIOTTO",
-                  textAlign: TextAlign.center,
-                  style: style.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold)),
-              SizedBox(height: 45.0),
-              usernameField,
-              SizedBox(height: 25.0),
-              passwordField,
-              SizedBox(height: 10.0,),
-              resetPassword,
-              SizedBox(height: 55.0,),
-              loginButon,
-              SizedBox(height: 50.0,),
-              registrationButon,
-            ],
-          ),
+                SizedBox(height: 10.0),
+                Text("CALCIOTTO",
+                    textAlign: TextAlign.center,
+                    style: style.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                SizedBox(height: 35.0),
+                usernameField,
+                SizedBox(height: 15.0),
+                passwordField,
+                SizedBox(height: 10.0,),
+                resetPassword,
+                SizedBox(height: 40.0,),
+                loginButon,
+                SizedBox(height: 20.0,),
+                registrationButon,
+              ],
+            ),
+          )
         ),
       ),
-      bottomNavigationBar: SizedBox(height: 70, child: appBar),
+      bottomNavigationBar: SizedBox(height: 50, child: appBar),
     );
   }
 }
