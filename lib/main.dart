26a9b2f426d2eb13fc5f65e00ca61f3036927b45
Leonardo Menti <leonardo.menti@studenti.unimi.it>
2021-12-06 22:12:28 +0100

    defined routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/login.dart' ;
+import 'package:calciotto/views/all.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (context) => Login(),
+        '/signin': (context) => Signin(),
+      },
     );
   }
 }
